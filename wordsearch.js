6025d797537961486a551fea92b636ba5a06bd6e
Coltonb-boop <coltonbitz@gmail.com>
2022-08-03 17:37:34 -0600

    cleaned the repo up a bit

--- wordsearch.js
+++ wordsearch.js
@@ -1,15 +1,4 @@
 // Colton and Mustafa
-// const arr = [ 
-// ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-// ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-// ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-// ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-// ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-// ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-// ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-// ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-// ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-// ];
 
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
@@ -26,17 +15,7 @@ const wordSearch = (letters, word) => {
         if (l.includes(word)) return true;
     }
 
-    // search vertically
-    // nested for loop
-    // for row
-    //  for column
-    //      string += array[row][column];
-    //      if string includes word return true
-
-
     return false;
 }
-
-// wordSearch(arr, "ssss")
 
 module.exports = wordSearch
